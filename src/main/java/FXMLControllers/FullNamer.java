d5a8c4fb4d58e42cd127b2856ffb1d4ea380a53b
jyxiao1 <jyxiao@wpi.edu>
2018-09-12 10:25:42 +0200

    Add keywords for full namer keywords CSV updated

--- src/main/java/FXMLControllers/FullNamer.java
+++ src/main/java/FXMLControllers/FullNamer.java
@@ -1,5 +1,6 @@
 package FXMLControllers;
 
+import Singletons.FXMLManager;
 import Types.ExperimentManager;
 import Types.KeywordManager;
 import Utilities.AutocompleteTextField;
@@ -10,6 +11,7 @@ import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTextArea;
 import com.jfoenix.controls.JFXTextField;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.geometry.Insets;
@@ -17,6 +19,9 @@ import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextArea;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Paint;
 import javafx.scene.text.Font;
@@ -27,6 +32,7 @@ import javax.swing.*;
 import java.awt.datatransfer.*;
 import java.awt.Toolkit;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.time.LocalDate;
@@ -55,7 +61,7 @@ public class FullNamer extends ScreenController implements Initializable, ITypeO
     private JFXButton backButton;
 
     @FXML
-    private VBox listOfKeywords;
+    private VBox vboxOfKeywords;
 
     @FXML
     private JFXButton addKeywordButton;
@@ -117,10 +123,10 @@ public class FullNamer extends ScreenController implements Initializable, ITypeO
             fname.append("_");
             try {
                 experimentShorthand = ExperimentManager.getInstance().getExperimentByName("long",experimentType.getText()).getShortName();
+                fname.append(experimentShorthand);
             } catch (NameNotFoundException e1) {
                 e1.printStackTrace();
             }
-            fname.append(experimentShorthand);
         }
 
         if(trialNumber.getText() != null)
@@ -128,27 +134,55 @@ public class FullNamer extends ScreenController implements Initializable, ITypeO
             fname.append("_");
             fname.append(trialNumber.getText());
         }
-
-
-        fileName.setText(fname.toString());
-
+        
         for(AutocompleteTextField autocompleteTextField : listofkeywords)
         {
-
+            System.out.println("foundkeyword");
+            if(autocompleteTextField.getText() != null && autocompleteTextField.getText().length() != 0)
+            {
+                System.out.println("stringstringstring");
+                String keyword;
+                fname.append("_");
+                try {
+                    keyword = KeywordManager.getInstance().getKeywordByName("long",autocompleteTextField.getText()).getShortName();
+                    fname.append(keyword);
+                } catch (NameNotFoundException e1) {
+                    e1.printStackTrace();
+                }
+                System.out.println(fname.toString());
+            }
         }
+        System.out.println(fname.toString());
         return fname.toString();
     }
 
     @FXML
     public void addKeyword(ActionEvent e) throws IOException{
+        FXMLManager fxmlManager = FXMLManager.getInstance();
         AutocompleteTextField textField = new AutocompleteTextField();
         textField.setPromptText("Choose Keyword");
         textField.setAlignment(Pos.BASELINE_LEFT);
         textField.setLabelFloat(true);
         textField.setUnFocusColor(Paint.valueOf("#0d004d"));
         textField.setFont(new Font("Palatino Linotype", 20));
-        listOfKeywords.getChildren().add(textField);
+        //fxmlManager.setSearchDirectory(System.getProperty("user.dir") + "/src/main/resources/");
+        //fxmlManager.loadFXML("FXML/fullNamer.fxml");
+        //File imageFile = fxmlManager.getFXMLNode("FXML/fullNamer.fxml");
+
+        //Image image = new Image(imageFile.toURI().toString());
+        //JFXButton jfxButton = new JFXButton("  ", new ImageView(image));
+        //jfxButton.setPrefSize(30,30);
         listofkeywords.add(textField);
+        HBox hbox = new HBox();
+        hbox.setSpacing(10);
+
+        /*jfxButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override public void handle(ActionEvent e) {
+                //listofkeywords.remove()
+            }
+        });*/
+
+        vboxOfKeywords.getChildren().add(textField);
         numKeywords++;
         onTypeUpdate();
     }
@@ -159,10 +193,13 @@ public class FullNamer extends ScreenController implements Initializable, ITypeO
     }
 
     @FXML
-    public void copyFileToClipboard(ActionEvent e) throws IOException{String myString = updateName();
-        StringSelection stringSelection = new StringSelection(myString);
+    public void copyFileToClipboard(ActionEvent e) throws IOException{
+        String nameToCopy = updateName();
+
+        StringSelection stringSelection = new StringSelection(nameToCopy);
         Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
         clipboard.setContents(stringSelection, null);
+        fileName.setText(nameToCopy);
         int currTrial = Integer.parseInt(trialNumber.getText());
         currTrial++;
         trialNumber.setText(String.valueOf(currTrial));
