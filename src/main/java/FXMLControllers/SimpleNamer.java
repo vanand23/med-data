c9d0cb90e0546813e8c2db0f02b14fdd31e8f1cd
Carlisle Ferguson <carlisleferguson@gmail.com>
2018-09-13 16:42:33 +0200

    Simple namer intialization

--- src/main/java/FXMLControllers/SimpleNamer.java
+++ src/main/java/FXMLControllers/SimpleNamer.java
@@ -6,19 +6,27 @@ import com.jfoenix.controls.JFXToggleButton;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.DatePicker;
+import javafx.util.StringConverter;
 
 import java.awt.*;
 import java.awt.datatransfer.Clipboard;
 import java.awt.datatransfer.StringSelection;
 import java.io.IOException;
+import java.net.URL;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ResourceBundle;
 
-public class SimpleNamer extends ScreenController {
+public class SimpleNamer extends ScreenController implements Initializable {
 
     @FXML
-    private JFXTextField experimentDate;
+    private DatePicker experimentDate;
 
     @FXML
     private JFXTextField trialNumber;
+
+    @FXML JFXTextField sampleNumber;
 
     @FXML
     private JFXButton copyButton;
@@ -26,6 +34,38 @@ public class SimpleNamer extends ScreenController {
     @FXML
     private JFXToggleButton switchNamers;
 
+    @Override
+
+    public void initialize(URL location, ResourceBundle resources){
+
+        String pattern = "dd-MM-yyyy";
+        experimentDate.setPromptText(pattern.toLowerCase());
+        experimentDate.setConverter(new StringConverter<LocalDate>() {
+            DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern(pattern);
+
+            @Override
+            public String toString(LocalDate date) {
+                if (date != null) {
+                    return dateFormatter.format(date);
+                } else {
+                    return "";
+                }
+            }
+
+            @Override
+            public LocalDate fromString(String string) {
+                if (string != null && !string.isEmpty()) {
+                    return LocalDate.parse(string, dateFormatter);
+                } else {
+                    return null;
+                }
+            }
+        });
+
+        trialNumber.setText("0");
+       sampleNumber.setText("0");
+       experimentDate.setValue(LocalDate.now());
+    }
 
     @FXML
     public void copy(ActionEvent e) throws IOException {
@@ -34,6 +74,7 @@ public class SimpleNamer extends ScreenController {
         Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
         clipboard.setContents(stringSelection, null);
     }
+
 
 
 
