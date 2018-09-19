f92f6ea5bca208142ad00702f0db4baa61792469
jyxiao1 <jyxiao@wpi.edu>
2018-09-19 21:42:56 +0200

    All screens are draggable

--- src/main/java/Launcher/Launcher.java
+++ src/main/java/Launcher/Launcher.java
@@ -39,20 +39,14 @@ public class Launcher extends Application {
 
         Parent root = fxmlManager.getFXMLNode("FXML/fullNamer.fxml");
         primaryStage.initStyle(StageStyle.TRANSPARENT);
-        root.setOnMousePressed(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent event) {
-                xOffset = event.getSceneX();
-                yOffset = event.getSceneY();
-            }
+        root.setOnMousePressed(event -> {
+            xOffset = event.getSceneX();
+            yOffset = event.getSceneY();
         });
 
-        root.setOnMouseDragged(new EventHandler<MouseEvent>(){
-            @Override
-                    public void handle(MouseEvent event){
-                primaryStage.setX(event.getScreenX()-xOffset);
-                primaryStage.setY(event.getScreenY() - yOffset);
-            }
+        root.setOnMouseDragged(event -> {
+            primaryStage.setX(event.getScreenX() - xOffset);
+            primaryStage.setY(event.getScreenY() - yOffset);
         });
 
         //primaryStage.setX(width - 400);
